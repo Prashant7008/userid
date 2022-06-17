5b43a38018550ed61e4b511545668aeb5adf150c
prashant <prashantbarik8457@gmail.com>
2022-06-17 23:44:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import Home from './components/pages/home';
+import Contact from './components/pages/contact';
+import About from './components/pages/About';
+import Navbar from './components/pages/layout/navbar';
+import {BrowserRouter,Route, Routes,} from 'react-router-dom';
+function App() {
+  return (
+   <BrowserRouter>
+     <div className="App">
+        <Navbar/>
+<Routes>
+  <Route  path="/" element={<Home/>}/>
+  <Route  path='/about' element={<About/>}/>
+  <Route  path='/contact' element={<Contact/>}/>
+</Routes>
+
+     
+    
+    </div>
+    
+   </BrowserRouter>
+  );
+}
+
+export default App;
